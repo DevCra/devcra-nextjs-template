29083208981f869003e29de213c659a5f8c45641
sy-chung <0sinf@devcra.com>
2025-09-25 11:17:59 +0900

    chore: Remove middleware (not used) and Add env in gitignore

--- src/middleware.ts
+++
@@ -1,5 +0,0 @@
-// import NextAuth from "next-auth";
-// import { defaultAuth } from "@/lib/auth";
-
-// export const { auth: middleware } = NextAuth(defaultAuth);
-export { auth as middleware } from "@/auth";
